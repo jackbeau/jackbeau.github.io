8d8905d84fd70653cfdf1eddcb2652267874833a
jackbeau <jackbeau@users.noreply.github.com>
2021-07-03 21:03:34 +0000

    deploy: 14502361b2a397da42bc35cda3dbfce1e7adaca8

---
+++ precache-manifest.cb9c0d263e3664fbd84a3b3d5c0f7cd4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e3cbf0ae29e3dc9870f05f8e8370aa",
+    "url": "/coming-soon/index.html"
+  },
+  {
+    "revision": "b731f19a127c80c2746f",
+    "url": "/coming-soon/static/css/main.ec639678.chunk.css"
+  },
+  {
+    "revision": "ed93eeddf9369950c440",
+    "url": "/coming-soon/static/js/2.58aaed71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coming-soon/static/js/2.58aaed71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b731f19a127c80c2746f",
+    "url": "/coming-soon/static/js/main.992cd37c.chunk.js"
+  },
+  {
+    "revision": "7ac9359f873fa00efc04",
+    "url": "/coming-soon/static/js/runtime-main.7af92772.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/coming-soon/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/coming-soon/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/coming-soon/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/coming-soon/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/coming-soon/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/coming-soon/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/coming-soon/static/media/twitter.a50716dd.svg"
+  }
+]);
